soqucoin/soqucoin

commit c2fcf3ecd4edadcb991edfac035b827010add576

Improve Qt payment server IPC
Add a unique suffix to the IPC server name before hashing, to avoid
things like network collisions or attempts to reuse old sockets on
restart.

Remove now-superfluous code that removed the old socket after a crash.

src/qt/paymentserver.cpp
@@ -11,6 +11,7 @@
 #include "base58.h"
 #include "chainparams.h"
 #include "policy/policy.h"
+#include "random.h"
 #include "ui_interface.h"
 #include "util.h"
 #include "wallet/wallet.h"
@@ -55,6 +56,7 @@ const char* BIP70_MESSAGE_PAYMENTREQUEST = "PaymentRequest";
 const char* BIP71_MIMETYPE_PAYMENT = "application/bitcoin-payment";
 const char* BIP71_MIMETYPE_PAYMENTACK = "application/bitcoin-paymentack";
 const char* BIP71_MIMETYPE_PAYMENTREQUEST = "application/bitcoin-paymentrequest";
+const int IPC_SOCKET_HASH = GetRandInt(INT_MAX);
 
 struct X509StoreDeleter {
       void operator()(X509_STORE* b) {
@@ -84,7 +86,7 @@ static QString ipcServerName()
     // Note that GetDataDir(true) returns a different path
     // for -testnet versus main net
     QString ddir(GUIUtil::boostPathToQString(GetDataDir(true)));
-    name.append(QString::number(qHash(ddir)));
+    name.append(QString::number(qHash(ddir, IPC_SOCKET_HASH)));
 
     return name;
 }
@@ -313,9 +315,6 @@ PaymentServer::PaymentServer(QObject* parent, bool startLocalServer) :
         parent->installEventFilter(this);
 
     QString name = ipcServerName();
-
-    // Clean up old socket leftover from a crash:
-    QLocalServer::removeServer(name);
 
     if (startLocalServer)
     {

commit 28e062156b356a8a2b3e6b12285b98cec5a1daee

qt: fix qt4 compile error
c2fcf3ecd introduced a compile error on qt4 because qHash only takes
an int argument from qt5+. This fixes that by converting the seed
into a string and both prepending and appending the seed to the
datadir whenever compiling on qt4.

src/qt/paymentserver.cpp
@@ -87,7 +87,12 @@ static QString ipcServerName()
     // Note that GetDataDir(true) returns a different path
     // for -testnet versus main net
     QString ddir(GUIUtil::boostPathToQString(GetDataDir(true)));
+#if QT_VERSION >= 0x050000
     name.append(QString::number(qHash(ddir, IPC_SOCKET_HASH)));
+#else
+    QString rseed = QString::number(IPC_SOCKET_HASH);
+    name.append(QString::number(qHash(rseed + ddir + rseed)));
+#endif //QT_VERSION >= 0x050000
 
     return name;
 }